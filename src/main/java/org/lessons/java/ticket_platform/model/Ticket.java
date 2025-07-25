30da325805e2b5462ff2820983da62aac4e526ff
AleDerosa1 <derosaalessandro018@gmail.com>
2025-07-25 22:05:15 +0200

    continuo

--- src/main/java/org/lessons/java/ticket_platform/model/Ticket.java
+++ src/main/java/org/lessons/java/ticket_platform/model/Ticket.java
@@ -1,5 +1,29 @@
 package org.lessons.java.ticket_platform.model;
 
+import org.springframework.data.annotation.Id;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tickets")
 public class Ticket {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+
+    @NotBlank(message = "The tilte must not be blank, empty or null")
+    @Size(min = 4, message = "Title must contain at least 4 characters")
+    private String title;
+
+    
+
+
     
 }
